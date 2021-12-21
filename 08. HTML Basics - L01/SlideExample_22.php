ed8e3429c542eb165787a4e9f5d40697fc136c3f
Layla <58508443+LaylaAbab@users.noreply.github.com>
2021-12-21 22:40:50 +0300

    Project files upload
    uploaded the project files.

---
+++ 08. HTML Basics - L01/SlideExample_22.php
@@ -0,0 +1,12 @@
+<html>
+	<body>
+		<?php
+			
+			Function printName($x){
+				echo "Hello".$x;
+			}
+			
+			printName("Najla");
+		?>
+	</body>
+</html>
---
+++ 08. HTML Basics - L01/phpExample.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> PHP Example </title>
+	<meta charset="UTF-8"/>
+</head>
+<body>
+	<?php
+		echo "Hello World!";
+	?>
+</body>
+</html>
